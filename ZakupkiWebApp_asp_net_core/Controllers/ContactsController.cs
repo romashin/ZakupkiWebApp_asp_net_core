e79e455ccecdbed86dd737facbd5bb362831f8c1
Алексей <@WIN-6RA3CTFP23D>
2025-06-16 22:25:38 +0300

    Добавьте файлы проекта.

---
+++ ZakupkiWebApp_asp_net_core/Controllers/ContactsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ZakupkiWebApp_asp_net_core.Models;
+
+namespace ZakupkiWebApp_asp_net_core.Controllers
+{
+    public class ContactsController : Controller
+    {
+        public IActionResult Index() // contacts/about
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Check(Contact contact) // contacts/about
+        {
+            if (ModelState.IsValid)
+            {
+                return Redirect("/");
+            }
+            return View("Index");
+
+        }
+
+    }
+}
---
+++ ZakupkiWebApp_asp_net_core/Models/Contact.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+
+namespace ZakupkiWebApp_asp_net_core.Models
+{
+    public class Contact
+    {
+        [Display(Name = "Введите имя")]
+        [Required(ErrorMessage = "Вам нужно ввести имя")]
+       
+        public string Name { get; set; }
+        
+        [Display(Name = "Введите фамилию")]
+        [Required(ErrorMessage = "Вам нужно ввести фамилию")]
+        public string Surname { get; set; }
+
+        [Display(Name = "Введите возраст")]
+        [Required(ErrorMessage = "Вам нужно ввести возраст")]
+        public int Age { get; set; }
+
+        [Display(Name = "Введите адрес электронной почты")]
+        [Required(ErrorMessage = "Вам нужно ввести адрес электронной почты")]
+        public string Email { get; set; }
+
+        [Display(Name = "Введите сообщение")]
+        [Required(ErrorMessage = "Вам нужно ввести текст сообщения")]
+        [StringLength(30, ErrorMessage = "Текст менее 30 символов")]
+        public string Message { get; set; }
+    }
+}
